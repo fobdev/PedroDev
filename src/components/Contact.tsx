cd344e1249dc5f8e41d6284d0cecf9576aa6feaa
Fobenga <silvapedro55@gmail.com>
2021-10-02 14:36:26 -0300

    firebase implementation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,28 +9,22 @@ import {
     DialogContentText,
     DialogActions,
 } from "@mui/material";
-
 import LoadingButton from "@mui/lab/LoadingButton";
-
 import emailJS from "emailjs-com";
 import React, { useState } from "react";
 import { Box } from "@mui/system";
+import { mainContainer } from "./styles/Contact";
+
+const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 interface userInfo {
     email: string;
     subject: string;
     message: string;
 }
-
 export default function Contact() {
-    const main_color = "#49a6e9";
     emailJS.init("user_d1mS4JOxBA5704aYP1SJ4");
-
-    const [form, setForm] = useState<userInfo>({
-        email: "",
-        subject: "",
-        message: "",
-    });
 
     let emailObject = {
         email: "",
@@ -38,9 +32,11 @@ export default function Contact() {
         message: "",
     };
 
+    const [form, setForm] = useState<userInfo>(emailObject);
     const [popup, setPopup] = useState(false);
     const [popup_error, setPopup_error] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [buttonEnabled, setButtonEnabled] = useState(false);
 
     const handlePopup = (state: string) => {
         switch (state) {
@@ -60,6 +56,11 @@ export default function Contact() {
     };
 
     const handleChange = (prop: keyof userInfo) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (prop === "email") {
+            if (emailRegex.test(event.target.value)) setButtonEnabled(true);
+            else setButtonEnabled(false);
+        }
+
         setForm({ ...form, [prop]: event.target.value });
     };
 
@@ -71,95 +72,49 @@ export default function Contact() {
 
         emailJS.send("default_service", "template_tis81xk", emailObject).then(
             function (response) {
-                setLoading(false);
+                console.log(response);
                 handlePopup("send_success");
+                setLoading(false);
             },
             function (error) {
+                console.log(error);
                 setLoading(false);
                 handlePopup("error");
-                console.log("FAILED...", error);
+                console.log(error);
             }
         );
     };
 
     return (
-        <Container
-            sx={{
-                alignItems: "center",
-                display: "flex",
-                justifyContent: "center",
-                height: "100vh",
-                columnGap: "10em",
-                "@media only screen and (max-width: 780px)": {
-                    ".dev-container": {
-                        display: "none",
-                    },
-                },
-
-                backgroundColor: "white",
-            }}
-        >
-            <Box
-                sx={{
-                    ".email": {
-                        gridArea: "em",
-                    },
-                    ".subject": {
-                        gridArea: "sj",
-                    },
-                    ".message": {
-                        gridArea: "ms",
-                    },
-                    ".title": {
-                        gridArea: "tt",
-                        fontSize: "3em",
-                        fontWeight: "200",
-                    },
-                    ".subtitle": {
-                        fontWeight: "100",
-                        gridArea: "if",
-                        alignSelf: "center",
-                    },
-                    ".loading-button": {
-                        borderRadius: "5em",
-                        gridArea: "bs",
-                        color: main_color,
-                        borderColor: "main_color",
-                    },
-                    backgroundColor: "white",
-                    display: "grid",
-                    justifyContent: "center",
-                    gridGap: "1em",
-                    gridTemplateAreas: `"tt tt"
-                                        "em sj"
-                                        "ms ms"
-                                        "if  bs"`,
-                    "@media only screen and (max-width: 780px)": {
-                        ".title": {
-                            fontSize: "2.5em",
-                            fontWeight: "200",
-                        },
-                    },
-                }}
-            >
-                <Typography className="title" sx={{}}>
-                    Contact Me
+        <Container sx={mainContainer}>
+            <Box className="dev-box" alignSelf="center">
+                <Typography variant="h3">{"¯\\_(ツ)_/¯"}</Typography>
+                <Typography variant="h5" marginTop="1em">
+                    Sorry, nothing to see here... yet!
                 </Typography>
+                <Typography variant="subtitle2">
+                    ...but the e-mail form on the right is working fine, feel free to use it!
+                </Typography>
+            </Box>
+            <Box className="contact-box">
+                <Typography className="title">Contact Me</Typography>
                 <TextField
                     className="email"
                     onChange={handleChange("email")}
                     value={form.email}
+                    type="email"
                     id="email"
                     variant="outlined"
+                    required={true}
                     label="E-mail"
                 />
-
                 <TextField
                     className="subject"
                     onChange={handleChange("subject")}
                     value={form.subject}
                     id="subject-field"
                     variant="outlined"
+                    required={true}
                     label="Subject"
                 />
                 <TextField
@@ -170,12 +125,18 @@ export default function Contact() {
                     rows={4}
                     id="message-field"
                     variant="outlined"
-                    label="Say something..."
+                    required={true}
                 />
-                <Typography className="subtitle" variant="subtitle2">
-                    You'll be sending a e-mail to silvapedro55@gmail.com
-                </Typography>
-
+                <LoadingButton
+                    className="loading-button"
+                    loading={loading}
+                    disabled={!buttonEnabled}
+                    variant="outlined"
+                    loadingIndicator="Sending..."
+                    onClick={handleEmailSending}
+                >
+                    Send
+                </LoadingButton>
                 <Dialog
                     open={popup}
                     aria-labelledby="alert-dialog-title"
@@ -215,47 +176,6 @@ export default function Contact() {
                         </Button>
                     </DialogActions>
                 </Dialog>
-
-                <LoadingButton
-                    className="loading-button"
-                    loading={loading}
-                    variant="outlined"
-                    loadingIndicator="Sending..."
-                    onClick={handleEmailSending}
-                >
-                    Send
-                </LoadingButton>
-            </Box>
-            <Box className="dev-container" alignSelf="center">
-                <Typography
-                    variant="h3"
-                    sx={{ fontFamily: "Roboto", fontWeight: "100", textAlign: "center" }}
-                >
-                    {"¯\\_(ツ)_/¯"}
-                </Typography>
-                <Typography
-                    variant="h5"
-                    sx={{
-                        fontFamily: "Roboto",
-                        fontWeight: "200",
-                        textAlign: "center",
-                        marginTop: "1em",
-                    }}
-                >
-                    Sorry, nothing to see here... yet!
-                </Typography>
-                <Typography
-                    variant="subtitle2"
-                    sx={{
-                        fontFamily: "Roboto",
-                        fontWeight: "300",
-                        textAlign: "center",
-                    }}
-                >
-                    My website is under development, but the mailbox on the left is working fine!
-                    <br />
-                    Feel free to message me :^)
-                </Typography>
             </Box>
         </Container>
     );
---
+++ src/components/styles/Contact.ts
@@ -0,0 +1,64 @@
+import { SxProps, Theme } from "@mui/system";
+const main_color = "#49a6e9";
+
+export const mainContainer: SxProps<Theme> = {
+    backgroundColor: "white",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    height: "100vh",
+    columnGap: "10em",
+
+    ".dev-box": {
+        ".MuiTypography-root": {
+            fontFamily: "Roboto",
+            fontWeight: "200",
+            textAlign: "center",
+        },
+    },
+
+    "@media only screen and (max-width: 780px)": {
+        ".dev-box": {
+            display: "none",
+        },
+    },
+
+    ".contact-box": {
+        ".email": {
+            borderColor: main_color,
+            width: "15em",
+            gridArea: "em",
+        },
+        ".subject": {
+            width: "20em",
+            gridArea: "sj",
+        },
+        ".message": {
+            gridArea: "ms",
+        },
+        ".title": {
+            gridArea: "tt",
+            fontSize: "3em",
+            fontWeight: "200",
+        },
+        ".loading-button": {
+            gridArea: "bs",
+            width: "10em",
+            borderRadius: "5em",
+        },
+        backgroundColor: "white",
+        display: "grid",
+        justifyContent: "center",
+        gridGap: "1em",
+        gridTemplateAreas: `"tt tt tt"
+                            "em sj sj"
+                            "ms ms ms"
+                            ". . bs"`,
+        "@media only screen and (max-width: 780px)": {
+            ".title": {
+                fontSize: "2.5em",
+                fontWeight: "200",
+            },
+        },
+    },
+};
