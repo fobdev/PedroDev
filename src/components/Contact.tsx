31ea9375be5486367721a2537a76d1551d323d1c
Fobenga <silvapedro55@gmail.com>
2021-10-02 17:21:30 -0300

    added dialog for empty boxes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,12 +8,13 @@ import {
     DialogContent,
     DialogContentText,
     DialogActions,
+    TextFieldProps,
 } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import emailJS from "emailjs-com";
-import React, { useState } from "react";
 import { Box } from "@mui/system";
 import { mainContainer } from "./styles/Contact";
+import React, { useState, useRef, useEffect } from "react";
 
 const emailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -33,8 +34,13 @@ export default function Contact() {
     };
 
     const [form, setForm] = useState<userInfo>(emailObject);
+    const emailRef = useRef<TextFieldProps>();
+    const messageRef = useRef<TextFieldProps>();
+    const subjectRef = useRef<TextFieldProps>();
+
     const [popup, setPopup] = useState(false);
     const [popup_error, setPopup_error] = useState(false);
+    const [popup_emptyFields, setPopup_emptyFields] = useState(false);
     const [loading, setLoading] = useState(false);
     const [buttonEnabled, setButtonEnabled] = useState(false);
 
@@ -43,25 +49,28 @@ export default function Contact() {
             case "send_success":
                 setPopup(true);
                 break;
-            case "closing":
-                setPopup(false);
-                setPopup_error(false);
-                break;
             case "error":
                 setPopup_error(true);
+                break;
+            case "emptyfields":
+                setPopup_emptyFields(true);
+                break;
+            case "closing":
+                setPopup_emptyFields(false);
+                setPopup(false);
+                setPopup_error(false);
                 break;
             default:
                 break;
         }
     };
 
-    const handleChange = (prop: keyof userInfo) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (prop: keyof userInfo) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({ ...form, [prop]: e.target.value });
         if (prop === "email") {
-            if (emailRegex.test(event.target.value)) setButtonEnabled(true);
+            if (emailRegex.test(e.target.value)) setButtonEnabled(true);
             else setButtonEnabled(false);
         }
-
-        setForm({ ...form, [prop]: event.target.value });
     };
 
     const handleEmailSending = () => {
@@ -70,19 +79,24 @@ export default function Contact() {
         emailObject.subject = form.subject;
         emailObject.message = form.message;
 
-        emailJS.send("default_service", "template_tis81xk", emailObject).then(
-            function (response) {
-                console.log(response);
-                handlePopup("send_success");
-                setLoading(false);
-            },
-            function (error) {
-                console.log(error);
-                setLoading(false);
-                handlePopup("error");
-                console.log(error);
-            }
-        );
+        if (!emailObject.email || !emailObject.subject || !emailObject.message) {
+            setLoading(false);
+            handlePopup("emptyfields");
+        } else {
+            emailJS.send("default_service", "template_tis81xk", emailObject).then(
+                function (response) {
+                    console.log(response);
+                    handlePopup("send_success");
+                    setLoading(false);
+                },
+                function (error) {
+                    console.log(error);
+                    setLoading(false);
+                    handlePopup("error");
+                    console.log(error);
+                }
+            );
+        }
     };
 
     return (
@@ -101,7 +115,8 @@ export default function Contact() {
                 <TextField
                     className="email"
                     onChange={handleChange("email")}
-                    value={form.email}
+                    defaultValue={form.email}
+                    inputRef={emailRef}
                     type="email"
                     id="email"
                     variant="outlined"
@@ -111,21 +126,22 @@ export default function Contact() {
                 <TextField
                     className="subject"
                     onChange={handleChange("subject")}
-                    value={form.subject}
+                    inputRef={subjectRef}
+                    defaultValue={form.subject}
                     id="subject-field"
                     variant="outlined"
-                    required={true}
                     label="Subject"
                 />
                 <TextField
                     className="message"
-                    value={form.message}
+                    defaultValue={form.message}
+                    inputRef={messageRef}
                     onChange={handleChange("message")}
                     multiline
                     rows={4}
+                    label="Say something"
                     id="message-field"
                     variant="outlined"
-                    required={true}
                 />
                 <LoadingButton
                     className="loading-button"
@@ -142,37 +158,55 @@ export default function Contact() {
                     aria-labelledby="alert-dialog-title"
                     aria-describedby="alert-dialog-description"
                 >
-                    <DialogTitle id="alert-dialog-title">{"E-mail sent!"}</DialogTitle>
+                    <DialogTitle id="alert-dialog-title">{"Thank you!"}</DialogTitle>
                     <DialogContent>
                         <DialogContentText id="alert-dialog-description">
-                            Thank you for contacting me, I will respond you soon :3
+                            I'll be reading your e-mail soon.
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={() => handlePopup("closing")} autoFocus>
-                            You're Welcome!
+                            Close
                         </Button>
                     </DialogActions>
                 </Dialog>
-
+                <Dialog
+                    open={popup_emptyFields}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Please fill all the fields."}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Looks like one or more fields is empty, please fill all of them.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => handlePopup("closing")} autoFocus>
+                            Ok
+                        </Button>
+                    </DialogActions>
+                </Dialog>
                 <Dialog
                     open={popup_error}
                     aria-labelledby="alert-dialog-title"
                     aria-describedby="alert-dialog-description"
                 >
                     <DialogTitle id="alert-dialog-title">
-                        {"Oops... Something happened :("}
+                        {"Oops... Something went wrong."}
                     </DialogTitle>
                     <DialogContent>
                         <DialogContentText id="alert-dialog-description">
                             An error occurred trying to send the e-mail.
                             <br />
-                            Check the e-mail field, maybe it's spelled incorrectly.
+                            Check the e-mail field, or maybe one of the text fields is blank.
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={() => handlePopup("closing")} autoFocus>
-                            Okay...
+                            Close
                         </Button>
                     </DialogActions>
                 </Dialog>
