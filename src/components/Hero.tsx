57e43a316733bbd61c2ad36810d372718df1fafb
Fobenga <silvapedro55@gmail.com>
2021-10-02 23:47:50 -0300

    changed all the css to another file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,97 +5,14 @@ import img1 from "../images/hero1.gif";
 import "./utils/animations.ts";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import ComputerRoundedIcon from "@mui/icons-material/ComputerRounded";
+import { mainHeroContainer } from "./styles/Hero";
 export default function Hero() {
-    const main_color = "#49a6e9";
     const { colorChange, float } = useStyles();
     const [animate, setAnimate] = useState(false);
 
     return (
         <div style={{ backgroundColor: "#eee" }}>
-            <Container
-                sx={{
-                    display: "flex",
-                    height: "100vh",
-                    justifyContent: "space-between",
-                    paddingBottom: "10vh",
-                    alignItems: "center",
-
-                    "@media only screen and (max-height: 580px)": {
-                        ".hello-text": {
-                            fontSize: "2em",
-                        },
-                        ".the-big-name": {
-                            fontSize: "3em",
-                        },
-                    },
-                    ".the-big-name, .description, .subtitle, .hello-text": {
-                        fontFamily: "Roboto",
-                        fontWeight: "200",
-                        textAlign: "left",
-                    },
-
-                    ".hello-text": {
-                        position: "sticky",
-                        fontWeight: "bold",
-                    },
-
-                    ".description": {
-                        fontSize: "2em",
-
-                        span: {
-                            textShadow: "0px 0px 2px #000",
-                        },
-                    },
-
-                    ".subtitle": {
-                        fontSize: "1.2em",
-                    },
-
-                    ".buttons-box": {
-                        display: "flex",
-                        marginTop: "3em",
-                        justifyContent: "left",
-                        columnGap: "5em",
-
-                        ".work-button, .contact-button": {
-                            padding: "1em 4em",
-                            borderRadius: "5em",
-                            backgroundColor: "white",
-                            color: main_color,
-                        },
-                    },
-                    ".box-image": {
-                        img: {
-                            width: "500px",
-                            borderRadius: "5em",
-                        },
-                    },
-                    "@media only screen and (max-width: 780px)": {
-                        height: "100vh",
-
-                        ".box-image": {
-                            display: "none",
-                        },
-
-                        ".hello-text, .the-big-name, .description, .subtitle": {
-                            textAlign: "center",
-                        },
-
-                        ".buttons-box": {
-                            display: "flex",
-                            flexDirection: "column",
-                            rowGap: "1em",
-                            marginTop: "3em",
-                            alignItems: "center",
-
-                            ".work-button, .contact-button": {
-                                width: "15rem",
-                                borderRadius: "5em",
-                            },
-                        },
-                    },
-                }}
-            >
+            <Container sx={mainHeroContainer}>
                 <Box>
                     <Fade in={true} timeout={{ enter: 2000 }}>
                         <Box>
@@ -129,11 +46,6 @@ export default function Hero() {
                                         left: 0,
                                         behavior: "smooth",
                                     });
-                                    // window.scrollTo({
-                                    //     top: 9999,
-                                    //     left: 0,
-                                    //     behavior: "smooth",
-                                    // })
                                 }}
                             >
                                 Contact
