95f72d61db2578e55ea2d1c6da790e458fc69253
Fobenga <silvapedro55@gmail.com>
2021-10-06 21:07:32 -0300

    uncommented the about page

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,10 @@ function App() {
                     <Contact />
                 </Route>
                 <Route exact path="/projects">
-                    <Typography variant="h3" fontWeight="200" textAlign="center" margin="1em">
+                    {/* <Typography variant="h3" fontWeight="200" textAlign="center" margin="1em">
                         This page still under development, come back later!
-                    </Typography>
-                    {/* <About /> */}
+                    </Typography> */}
+                    <About />
                     <Contact />
                 </Route>
                 <Route path="*">
