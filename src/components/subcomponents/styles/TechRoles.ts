4401a45a99c47b745cbf7474cf54dc2ba58d5b69
Fobenga <silvapedro55@gmail.com>
2021-10-08 18:17:02 -0300

    updates to the minibuttons styling

--- src/components/subcomponents/styles/TechRoles.ts
+++ src/components/subcomponents/styles/TechRoles.ts
@@ -1,25 +1,44 @@
 import { SxProps, Theme } from "@mui/system";
 
-export const mainTechRolesContainer: SxProps<Theme> = {
-    display: "flex",
-    alignItems: "center",
-    padding: ".7em",
-    borderRadius: "1em",
-    columnGap: "1em",
-    ".image": {
-        width: "24px",
-        height: "24px",
-    },
-    ".MuiTypography-root": {
-        alignSelf: "center",
-        padding: 0,
-        fontWeight: "400",
-    },
+export const mainTechRolesContainer = (selected: boolean) => {
+    let bgSelection = selected ? "#eef" : "#fff";
 
-    userSelect: "none",
-    cursor: "pointer",
-    "&:active": {
-        transition: ".06s",
-        boxShadow: "1px 1px 1px 1px rgba(0, 0, 0, .4)",
-    },
+    const Style: SxProps<Theme> = {
+        borderRadius: "1em",
+        alignItems: "center",
+        button: {
+            transition: ".5s",
+            background: bgSelection,
+            display: "flex",
+            padding: ".7em",
+            columnGap: "1em",
+            color: "inherit",
+            border: "none",
+            font: "inherit",
+            cursor: "pointer",
+            outline: "inherit",
+            borderRadius: "1em",
+        },
+        "button:hover": {
+            transition: ".5s",
+            background: "#eef",
+        },
+        ".image": {
+            width: "24px",
+            height: "24px",
+        },
+        ".MuiTypography-root": {
+            alignSelf: "center",
+            padding: 0,
+            fontWeight: "400",
+        },
+
+        userSelect: "none",
+        cursor: "pointer",
+        "&:active": {
+            transition: ".06s",
+            boxShadow: "1px 1px 1px 1px rgba(0, 0, 0, .4)",
+        },
+    };
+    return Style;
 };
