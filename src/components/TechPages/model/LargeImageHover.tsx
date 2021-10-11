dd3032f810e6a94c5a4868e3cff708b9df55ee24
fobdev <silvapedro55@gmail.com>
2021-10-11 19:49:09 -0300

    added prop for border rounding

--- src/components/TechPages/model/LargeImageHover.tsx
+++ src/components/TechPages/model/LargeImageHover.tsx
@@ -4,11 +4,12 @@ import { largeImageHoverMainBox } from "./styles/LargeImageHover";
 interface Props {
     image: any;
     link: string;
+    side: string;
 }
 
-export const LargeImageHover: React.FC<Props> = ({ image, link }) => {
+export const LargeImageHover: React.FC<Props> = ({ image, link, side }) => {
     return (
-        <Box sx={largeImageHoverMainBox}>
+        <Box sx={largeImageHoverMainBox(side)}>
             <a href={link} rel="noreferrer" target="_blank">
                 <img src={image} alt="" className="bigImage" />
             </a>
--- src/components/TechPages/model/styles/LargeImageHover.ts
+++ src/components/TechPages/model/styles/LargeImageHover.ts
@@ -1,24 +1,52 @@
 import { SxProps, Theme } from "@mui/system";
 
-export const largeImageHoverMainBox: SxProps<Theme> = {
-    display: "flex",
-    flexDirection: "column",
-    rowGap: "2em",
-    alignContent: "space-around",
-    marginBottom: "2em",
+export let largeImageHoverMainBox = (side: string) => {
+    const RightStyle: SxProps<Theme> = {
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "2em",
+        alignContent: "space-around",
+        marginBottom: "2em",
+        marginTop: "3em",
 
-    ".bigImage": {
-        maxWidth: "100%",
-        objectFit: "contain",
-        transition: "0.3s",
-        borderTopLeftRadius: "2em",
-        borderBottomLeftRadius: "2em",
-        "&:hover": {
+        ".bigImage": {
+            maxWidth: "100%",
+            objectFit: "contain",
             transition: "0.3s",
-            boxShadow: "-2px -2px 10px #aaa",
-            transform: "translate(5px, 5px)",
+            borderTopLeftRadius: "2em",
+            borderBottomLeftRadius: "2em",
+            "&:hover": {
+                transition: "0.3s",
+                boxShadow: "-2px -2px 10px #aaa",
+                transform: "translate(5px, 5px)",
+            },
         },
-    },
+    };
+
+    const LeftStyle: SxProps<Theme> = {
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "2em",
+        alignContent: "space-around",
+        marginBottom: "2em",
+        marginTop: "3em",
+
+        ".bigImage": {
+            maxWidth: "100%",
+            objectFit: "contain",
+            transition: "0.3s",
+            borderTopRightRadius: "2em",
+            borderBottomRightRadius: "2em",
+            "&:hover": {
+                transition: "0.3s",
+                boxShadow: "-2px -2px 10px #aaa",
+                transform: "translate(5px, 5px)",
+            },
+        },
+    };
+
+    if (side === "right") return RightStyle;
+    if (side === "left") return LeftStyle;
 };
 
 export default largeImageHoverMainBox;
