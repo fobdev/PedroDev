e365730946f5a74102a3978bf404bb30cd5a0123
fobdev <silvapedro55@gmail.com>
2021-10-11 09:33:12 -0300

    changed default pages for production

--- src/components/TechPages/TSPage.tsx
+++ src/components/TechPages/TSPage.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const TSPage: React.FC<Props> = ({ growIn, image }) => {
     return (
         <Project title="Typescript Projects" image={image} growIn={growIn}>
-            <Typography>This page still under development, please check back later.</Typography>
+            <Typography>This page is still under development, please come back later.</Typography>
         </Project>
     );
 };
