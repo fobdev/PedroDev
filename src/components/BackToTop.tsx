92c09beec21968a90d3a95702445ece3f36f0e8a
fobdev <silvapedro55@gmail.com>
2021-10-10 21:52:00 -0300

    refactored code for easier reading

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,43 +1,42 @@
-import React, { useState } from "react";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { useState } from "react";
 import { IconButton, Slide } from "@mui/material";
+import { Box, SxProps, Theme } from "@mui/system";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 export default function BackToTop() {
     const [scrolled, setScrolled] = useState(false);
-    window.onscroll = function () {
-        if (document.documentElement.scrollTop > 20) {
-            setScrolled(true);
-        } else setScrolled(false);
+
+    window.onscroll = () => {
+        return document.documentElement.scrollTop > 20 ? setScrolled(true) : setScrolled(false);
     };
+
+    const style: SxProps<Theme> = {
+        display: "flex",
+        position: "fixed",
+        bottom: "0",
+        left: "0",
+        margin: "0 0 2em 2em",
+        ".arrow": {
+            width: "2em",
+            height: "2em",
+        },
+    };
+
+    const handleClick = () => {
+        return window.scrollBy({
+            top: -999,
+            left: 0,
+            behavior: "smooth",
+        });
+    };
+
     return (
-        <div
-            style={{
-                display: "flex",
-                position: "fixed",
-                bottom: "0",
-                left: "0",
-                margin: "0 0 2em 2em",
-            }}
-        >
+        <Box sx={style}>
             <Slide direction="up" in={scrolled}>
-                <IconButton
-                    onClick={() =>
-                        window.scrollBy({
-                            top: -999,
-                            left: 0,
-                            behavior: "smooth",
-                        })
-                    }
-                    sx={{}}
-                >
-                    <ArrowUpwardIcon
-                        sx={{
-                            width: "2em",
-                            height: "2em",
-                        }}
-                    />
+                <IconButton onClick={handleClick}>
+                    <ArrowUpwardIcon className="arrow" />
                 </IconButton>
             </Slide>
-        </div>
+        </Box>
     );
 }
