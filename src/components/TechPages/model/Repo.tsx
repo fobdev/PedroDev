8a542fcc5af80bac1d0859e7b85df725ffe8fc1f
fobdev <silvapedro55@gmail.com>
2021-10-12 00:18:55 -0300

    added slide animations to body

--- src/components/TechPages/model/Repo.tsx
+++ src/components/TechPages/model/Repo.tsx
@@ -1,6 +1,7 @@
-import { Paper, Box, Typography } from "@mui/material";
+import { Paper, Box, Typography, Grow, Fade, Slide, easing } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { repoMainBox } from "./styles/Repo";
+import { useRef } from "react";
 
 interface Props {
     title: string;
@@ -10,20 +11,22 @@ interface Props {
 
 export const Repo: React.FC<Props> = ({ title, children, repo }) => {
     return (
-        <Box sx={repoMainBox}>
-            <Typography className="repo-title" variant="h4">
-                {title}
-            </Typography>
-            <Typography className="repo-description">{children}</Typography>
-            <a className="repo-link" href={repo} rel="noreferrer" target="_blank">
-                <Paper className="page-button" elevation={5}>
-                    <GitHubIcon className="gh-icon" />
-                    <Typography variant="h3" className="goto-repo">
-                        Go to repository
-                    </Typography>
-                </Paper>
-            </a>
-        </Box>
+        <Slide direction="up" in={true} timeout={{ enter: 500 }}>
+            <Box sx={repoMainBox}>
+                <Typography className="repo-title" variant="h4">
+                    {title}
+                </Typography>
+                <Typography className="repo-description">{children}</Typography>
+                <a className="repo-link" href={repo} rel="noreferrer" target="_blank">
+                    <Paper className="page-button" elevation={5}>
+                        <GitHubIcon className="gh-icon" />
+                        <Typography variant="h3" className="goto-repo">
+                            Go to repository
+                        </Typography>
+                    </Paper>
+                </a>
+            </Box>
+        </Slide>
     );
 };
 export default Repo;
