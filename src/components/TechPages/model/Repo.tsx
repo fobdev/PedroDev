8c6cdbd80a0dbae22944d42302b4a866c53e231d
fobdev <silvapedro55@gmail.com>
2021-10-12 14:41:39 -0300

    moved the big image to Repo function

--- src/components/TechPages/model/Repo.tsx
+++ src/components/TechPages/model/Repo.tsx
@@ -1,29 +1,49 @@
 import { Paper, Box, Typography, Slide } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import { repoMainBox } from "./styles/Repo";
+import { repoMainBoxStyling } from "./styles/Repo";
+import { largeImageHoverMainBox } from "./styles/LargeImageHover";
 
 interface Props {
     title: string;
     children: any;
-    repo: string;
+    repoLink: string;
+    side: string;
+    largeImageLink: string;
+    largeImage: any;
 }
 
-export const Repo: React.FC<Props> = ({ title, children, repo }) => {
+export const Repo: React.FC<Props> = ({
+    title,
+    children,
+    repoLink,
+    side,
+    largeImageLink,
+    largeImage,
+}) => {
     return (
         <Slide direction="up" in={true} timeout={{ enter: 1000, exit: 0 }}>
-            <Box sx={repoMainBox}>
-                <Typography className="repo-title" variant="h4">
-                    {title}
-                </Typography>
-                <Typography className="repo-description">{children}</Typography>
-                <a className="repo-link" href={repo} rel="noreferrer" target="_blank">
-                    <Paper className="page-button" elevation={5}>
-                        <GitHubIcon className="gh-icon" />
-                        <Typography variant="h3" className="goto-repo">
-                            Go to repository
-                        </Typography>
-                    </Paper>
-                </a>
+            <Box sx={repoMainBoxStyling(side)}>
+                <Box className="repobox-leftside">
+                    <Typography className="repo-title" variant="h4">
+                        {title}
+                    </Typography>
+                    <Typography className="repo-description">{children}</Typography>
+                    <a className="repo-link" href={repoLink} rel="noreferrer" target="_blank">
+                        <Paper className="page-button" elevation={5}>
+                            <GitHubIcon className="gh-icon" />
+                            <Typography variant="h3" className="goto-repo">
+                                Go to repository
+                            </Typography>
+                        </Paper>
+                    </a>
+                </Box>
+                <Box className="repobox-rightside">
+                    <Box sx={largeImageHoverMainBox(side)}>
+                        <a href={largeImageLink} rel="noreferrer" target="_blank">
+                            <img src={largeImage} alt="" className="bigImage" />
+                        </a>
+                    </Box>
+                </Box>
             </Box>
         </Slide>
     );
