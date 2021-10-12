9fe7e70603a7085e9228c32c52d04cb5de2c346b
fobdev <silvapedro55@gmail.com>
2021-10-12 14:38:28 -0300

    CodeLink implemented

---
+++ src/components/TechPages/model/CodeLink.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    link?: string;
+    children: any;
+}
+
+export const CodeLink: React.FC<Props> = ({ link, children }) => {
+    return (
+        <a href={link} rel="noreferrer" target="_blank">
+            <code>{children}</code>
+        </a>
+    );
+};
+export default CodeLink;
