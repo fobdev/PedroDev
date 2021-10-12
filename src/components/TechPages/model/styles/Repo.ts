93781f0c0a4a8b40fce5f35c96aeece3245d31b6
fobdev <silvapedro55@gmail.com>
2021-10-12 14:42:15 -0300

    made the image change sides more easily

--- src/components/TechPages/model/styles/Repo.ts
+++ src/components/TechPages/model/styles/Repo.ts
@@ -1,52 +1,68 @@
 import { SxProps, Theme } from "@mui/system";
 
-export const repoMainBox: SxProps<Theme> = {
-    display: "flex",
-    flexDirection: "column",
-    margin: "1em",
-    textAlign: "left",
+export const repoMainBoxStyling = (side: string) => {
+    let dir: any = side === "left" ? "rtl" : "ltr";
 
-    ".repo-title": {
-        fontWeight: "500",
-    },
-    ".repo-description": {
-        fontWeight: "300",
-    },
-    ".repo-link": {
-        marginTop: "1em",
-        alignSelf: "center",
-        display: "inherit",
-        textDecoration: "none",
-        color: "black",
+    const dirChange: SxProps<Theme> = {
+        direction: dir,
+    };
 
-        ".goto-repo": {
-            fontWeight: "200",
-            alignSelf: "center",
-            fontSize: "2em",
-        },
-        ".gh-icon": {
-            fontSize: "2em",
-        },
-    },
-
-    ".page-button": {
+    const mainStyle: SxProps<Theme> = {
         display: "flex",
-        transition: ".3s",
-        justifyContent: "center",
-        padding: "1em",
-        columnGap: "1em",
-        borderRadius: "1em",
-        userSelect: "none",
-        cursor: "pointer",
-        "&:hover": {
+        textAlign: "left",
+
+        ".repobox-rightside": {
+            width: "100%",
+        },
+
+        ".repobox-leftside": {
+            margin: "2em",
+            width: "100%",
+        },
+        ".repo-title": {
+            fontWeight: "500",
+        },
+        ".repo-description": {
+            fontWeight: "300",
+        },
+        ".repo-link": {
+            marginTop: "1em",
+            alignSelf: "center",
+            display: "inherit",
+            textDecoration: "none",
+            color: "black",
+
+            ".goto-repo": {
+                fontWeight: "200",
+                alignSelf: "center",
+                fontSize: "2em",
+            },
+            ".gh-icon": {
+                fontSize: "2em",
+            },
+        },
+
+        ".page-button": {
+            display: "flex",
             transition: ".3s",
-            background: "#eee",
+            justifyContent: "center",
+            padding: "1em",
+            columnGap: "1em",
+            borderRadius: "1em",
+            userSelect: "none",
+            cursor: "pointer",
+            "&:hover": {
+                transition: ".3s",
+                background: "#eee",
+            },
+            "&:active": {
+                transition: ".05s",
+                boxShadow: "1px 1px 1px 0 #000",
+            },
         },
-        "&:active": {
-            transition: ".05s",
-            boxShadow: "1px 1px 1px 0 #000",
-        },
-    },
+    };
+
+    return { ...dirChange, ...mainStyle };
 };
 
-export default repoMainBox;
+export default repoMainBoxStyling;
