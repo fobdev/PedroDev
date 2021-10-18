cf63dc2db65ad8798cbad816651120f86d16ac05
fobdev <silvapedro55@gmail.com>
2021-10-18 11:28:30 -0300

    remade header w new Mui components and smaller CSS

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,14 +1,13 @@
 /** MUI Components Imports */
 import {
-    Container,
     IconButton,
     Typography,
     Grow,
-    Box,
     List,
     Slide,
     ListItemButton,
     ListItemIcon,
+    Stack,
 } from "@mui/material";
 
 /** MUI Icons Imports */
@@ -18,128 +17,135 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import logo from "../images/logo.svg";
 
-/** Styling */
-import { mainHeadBox } from "./styles/Head";
-
 /** React */
 import { Link } from "react-router-dom";
+import { main_color } from "./styles/_globals";
+import { SxProps, Theme } from "@mui/system";
+
+const githubLink = "https://github.com/fobdev";
+const whatsappLink = "https://wa.me/+5599982207527";
+const linkedinLink = "https://www.linkedin.com/in/pedro-henrique-dev1997/";
 
 export default function Head() {
+    const handleToTopClick = () => {
+        return window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    const handleToBottomClick = () => {
+        return window.scrollTo({
+            top: document.body.scrollHeight,
+            left: 0,
+            behavior: "smooth",
+        });
+    };
+
+    const styling: SxProps<Theme> = {
+        backgroundColor: "rgba(255,255,255,0.70)",
+        backdropFilter: "blur(20px)",
+        ".link": {
+            textDecoration: "none",
+            color: "black",
+            div: { transition: "all .3s", "&:hover": { color: `${main_color}` } },
+        },
+        ".title": {
+            span: { color: `${main_color}` },
+        },
+        button: { color: `${main_color}` },
+        ".mobile-menu": {
+            display: "none",
+            color: main_color,
+            margin: 1.1,
+        },
+        "@media (max-width: 560px)": {
+            ul: {
+                display: "none",
+            },
+            ".mobile-menu": {
+                display: "inherit",
+            },
+        },
+    };
+
     return (
-        <Box sx={mainHeadBox}>
-            <Container className="main-container" maxWidth={false}>
-                <Box className="title-box">
-                    <Link
-                        className="title-link"
-                        to="/"
-                        onClick={() => {
-                            window.scrollTo({
-                                top: 0,
-                                behavior: "smooth",
-                            });
-                        }}
-                    >
-                        <Grow in={true} timeout={{ enter: 1000 }}>
-                            <img src={logo} alt="Website Logo" />
-                        </Grow>
-                        <Grow in={true} timeout={{ enter: 1000 }}>
-                            <Typography className="title">
-                                Pedro<span>Dev</span>
-                            </Typography>
-                        </Grow>
-                    </Link>
-                </Box>
-                <Box className="right-box">
-                    <IconButton className="menu">
-                        <MenuRoundedIcon />
-                    </IconButton>
-                    <List className="info-box">
-                        <Slide in={true} timeout={{ enter: 500 }}>
-                            <Link className="router-home" to="/">
-                                <ListItemButton
-                                    disableRipple
-                                    onClick={() => {
-                                        window.scrollTo({
-                                            top: 0,
-                                            behavior: "smooth",
-                                        });
-                                    }}
-                                >
-                                    Home
-                                </ListItemButton>
-                            </Link>
-                        </Slide>
-                        <Slide in={true} timeout={{ enter: 1500 }}>
-                            <Link className="router-projects" to="/projects">
-                                <ListItemButton
-                                    disableRipple
-                                    onClick={() => {
-                                        window.scrollTo({
-                                            top: 0,
-                                            behavior: "smooth",
-                                        });
-                                    }}
-                                >
-                                    Projects
-                                </ListItemButton>
-                            </Link>
-                        </Slide>
-                        <Slide in={true} timeout={{ enter: 2000 }}>
-                            <Box className="router-contact">
-                                <ListItemButton
-                                    disableRipple
-                                    onClick={() =>
-                                        window.scrollTo({
-                                            top: document.body.scrollHeight,
-                                            left: 0,
-                                            behavior: "smooth",
-                                        })
-                                    }
-                                >
-                                    Contact
-                                </ListItemButton>
-                            </Box>
-                        </Slide>
-                    </List>
+        <Stack
+            sx={styling}
+            position="sticky"
+            zIndex="1300"
+            top="0"
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+        >
+            <Link className="link" to="/" onClick={handleToTopClick}>
+                <Stack direction="row" spacing={0.1} margin={1} sx={{ img: { width: "24px" } }}>
                     <Grow in={true} timeout={{ enter: 1000 }}>
-                        <List className="links-box">
-                            <ListItemIcon>
-                                <a
-                                    href="https://wa.me/+5599982207527"
-                                    rel="noreferrer"
-                                    target="_blank"
-                                >
-                                    <IconButton className="whatsapp-button">
-                                        <WhatsAppIcon />
-                                    </IconButton>
-                                </a>
-                            </ListItemIcon>
-                            <ListItemIcon>
-                                <a
-                                    href="https://www.linkedin.com/in/pedro-henrique-dev1997/"
-                                    rel="noreferrer"
-                                    target="_blank"
-                                >
-                                    <IconButton className="linkedin-button">
-                                        <LinkedInIcon />
-                                    </IconButton>
-                                </a>
-                            </ListItemIcon>
-                            <ListItemIcon>
-                                <a
-                                    href="https://github.com/fobdev"
-                                    rel="noreferrer"
-                                    target="_blank"
-                                >
-                                    <IconButton className="github-button">
-                                        <GitHubIcon />
-                                    </IconButton>
-                                </a>
-                            </ListItemIcon>
-                        </List>
+                        <img src={logo} alt="Website Logo" />
                     </Grow>
-                </Box>
-            </Container>
-        </Box>
+                    <Grow in={true} timeout={{ enter: 1000 }}>
+                        <Typography className="title">
+                            Pedro<span>Dev</span>
+                        </Typography>
+                    </Grow>
+                </Stack>
+            </Link>
+            <IconButton>
+                <MenuRoundedIcon className="mobile-menu" />
+            </IconButton>
+            <List>
+                <Stack direction="row" spacing={1}>
+                    <Slide in={true} timeout={{ enter: 500 }}>
+                        <Link className="link" to="/">
+                            <ListItemButton onClick={handleToTopClick} disableRipple>
+                                Home
+                            </ListItemButton>
+                        </Link>
+                    </Slide>
+                    <Slide in={true} timeout={{ enter: 1000 }}>
+                        <Link className="link" to="/projects">
+                            <ListItemButton onClick={handleToTopClick} disableRipple>
+                                Projects
+                            </ListItemButton>
+                        </Link>
+                    </Slide>
+                    <Slide in={true} timeout={{ enter: 1500 }}>
+                        <Link className="link" to="#">
+                            <ListItemButton onClick={handleToBottomClick} disableRipple>
+                                Contact
+                            </ListItemButton>
+                        </Link>
+                    </Slide>
+                </Stack>
+            </List>
+            <List>
+                <Grow in={true} timeout={{ enter: 1000 }}>
+                    <Stack direction="row">
+                        <ListItemIcon>
+                            <a href={whatsappLink} rel="noreferrer" target="_blank">
+                                <IconButton className="icon-button">
+                                    <WhatsAppIcon />
+                                </IconButton>
+                            </a>
+                        </ListItemIcon>
+                        <ListItemIcon>
+                            <a href={linkedinLink} rel="noreferrer" target="_blank">
+                                <IconButton className="icon-button">
+                                    <LinkedInIcon />
+                                </IconButton>
+                            </a>
+                        </ListItemIcon>
+                        <ListItemIcon>
+                            <a href={githubLink} rel="noreferrer" target="_blank">
+                                <IconButton className="icon-button">
+                                    <GitHubIcon />
+                                </IconButton>
+                            </a>
+                        </ListItemIcon>
+                    </Stack>
+                </Grow>
+            </List>
+        </Stack>
     );
 }
--- src/components/styles/index.ts
+++ src/components/styles/index.ts
@@ -1,5 +1,4 @@
 export { mainAboutContainer, references } from "./About";
 export { mainContactContainer } from "./Contact";
 export { mainFooterBox } from "./Footer";
-export { mainHeadBox } from "./Head";
 export { mainHeroContainer } from "./Hero";
