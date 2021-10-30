c79507cd220173b2a860603356660c5228df8262
fobdev <silvapedro55@gmail.com>
2021-10-30 00:36:21 -0300

    added initial color pallete

--- src/pages/styles/_globals.ts
+++ src/pages/styles/_globals.ts
@@ -1,2 +1,14 @@
-export const page_padding = "5em 0 5em 0";
 export const main_color = "#49a6e9";
+
+export const pallete = {
+    black: {
+        background: "#000",
+        main_color: "#49a6e9",
+        backgroundAccent: "#444",
+    },
+    white: {
+        main_color: "#49a6e9",
+        background: "#fff",
+        backgroundAccent: "#eee",
+    },
+};
