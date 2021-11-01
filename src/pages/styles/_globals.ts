0d80ed3749d2f8e2973eb76ac06f8cc61f52aaa5
fobdev <silvapedro55@gmail.com>
2021-11-01 01:42:08 -0300

    New color variable

--- src/pages/styles/_globals.ts
+++ src/pages/styles/_globals.ts
@@ -1,15 +1,3 @@
 export const main_color = "#49a6e9";
-
-export const pallete = {
-    black: {
-        background: "#000",
-        main_color: "#49a6e9",
-        backgroundAccent: "#444",
-    },
-    white: {
-        main_color: "#49a6e9",
-        background: "#fff",
-        backgroundAccent:
-            "linear-gradient(90deg, rgba(241,253,255,1) 0%, rgba(255,238,250,1) 100%)",
-    },
-};
+export const main_gradient =
+    "linear-gradient(90deg, rgba(241,253,255,1) 0%, rgba(255,238,250,1) 100%)";
